ed89d8cbda94b848e873349ca91ddd4b9a00af55
Oleksandra Polenova <d21121972@gmail.com>
2022-03-31 18:58:54 +0300

    Merge

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login/Login';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-      </header>
+      <Login/> 
+      </header> 
     </div>
   );
 }
