3921d2854402e0da0b13b94f1dcd6277668a39e8
Oleksandra Polenova <d21121972@gmail.com>
2022-03-31 19:13:09 +0300

    Fix conflict from stash

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import './App.css';
-import Login from './components/Login/Login';
 
-const sendRequest = async (url, body) => {
-  const resp = await fetch(url, {
-    method: 'post',
-    body: JSON.stringify(body),
-    headers: { 'Content-Type': 'application/json' }
-  })
-  const data = await resp.json();
-  console.log(data);
-}
-
-const registerBody = { "email": "test@test.com", "name": "Alex", "password": "111" }
-sendRequest('/api/register', registerBody)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Login />
+        
       </header>
     </div>
   );
