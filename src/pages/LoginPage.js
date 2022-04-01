3a6a6898dc5bb0b4569b03186754d933e31d8266
Oleksandra Polenova <d21121972@gmail.com>
2022-04-01 17:35:49 +0300

    Task-2: Add sass, login component

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,35 +1,24 @@
 import { Fragment } from "react"
 import { Navigate } from "react-router-dom"
-import Login from "../components/Login/Login"
-import { sendRequest } from "../utils"
+import Login from "../components/authForm/authForm"
+//import { sendRequest } from "../utils"
 
 const LoginPage = ({ userData }) => {
 
-    const onClickButton = () => {
-        const registerBody = { "email": "test@test.com", "name": "Alex", "password": "111" }
-        sendRequest('/api/register', registerBody).then(
-            (data) => {
-                const { success } = data
-                if (success) {
-                    userData.setUser('Roma')    
-                } else {
-                    console.log('Error register',data.success);
-                }
-            }
-        )
-        //        setTimeout(() => sendRequest('/api/login', registerBody), 5000)
-        //        setTimeout(() => sendRequest('/api/forgot', registerBody), 10000)
+    const onSubmitHandler = () => {
 
-        
+        console.log('Login page onSubmit');
+
+
     }
 
 
     return (
         <Fragment>
-            { userData.user && <Navigate replace to="/" />} 
+            {userData.user && <Navigate replace to="/" />}
             <header>
-                <h1>Login page { userData.user } </h1>
-                <Login onSubmit={onClickButton}/>
+                <p>Login page {userData.user} </p>
+                <Login onSubmit={onSubmitHandler} />
             </header>
         </Fragment>
     )
