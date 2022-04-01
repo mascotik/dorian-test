36b412d496ecdfaf63f58ce8e8c5a59cdfaaec84
Oleksandra Polenova <d21121972@gmail.com>
2022-04-01 21:03:23 +0300

    Task-2: Refactor client

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,23 +1,15 @@
-import { Fragment } from "react"
-import { Navigate } from "react-router-dom"
-import Login from "../components/authForm/authForm"
-//import { sendRequest } from "../utils"
+import { Fragment } from "react";
+import { Navigate } from "react-router-dom";
+import Login from "../components/authForm/AuthForm";
 
 const LoginPage = ({ userData }) => {
 
-    const onSubmitHandler = () => {
-
-        console.log('Login page onSubmit');
-
-
-    }
-
+    const onSubmitHandler = (props) => userData.setUserCallback(props?.name);
 
     return (
         <Fragment>
             {userData.user && <Navigate replace to="/" />}
             <header>
-                <p>Login page {userData.user} </p>
                 <Login onSubmit={onSubmitHandler} />
             </header>
         </Fragment>
